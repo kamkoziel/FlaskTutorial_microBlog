5c8265e1ad5c4257926899fabe6b1b89c49e41a1
ropusch <kkoziel@outlook.com>
2019-10-20 22:57:02 +0200

    First part Ch. 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app.config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes
--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'exxtv2r3'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATEBASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
