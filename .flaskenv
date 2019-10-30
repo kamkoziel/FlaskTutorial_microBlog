6c32cb80a98bf8cd4313922318c7e796c90d2e9a
ropusch <kkoziel@outlook.com>
2019-10-30 07:48:07 +0100

    Tutorial Ch.6 Error Handling

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,7 @@
 FLASK_APP=microblog.py
+FLASK_DEBUG=1
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=mr.ropusch@gmail.com
+MAIL_PASSWORD=exxtv2r3
--- app/forms.py
+++ app/forms.py
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Zmien')
+
+    def __init__(self, orginal_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.orginal_username = orginal_username
+
+    def validate_username(self, username):
+        if username.data != self.orginal_username:
+            user = User.query.filter_by(username = self.username.data).first()
+            if user is not None:
+                raise ValidationError('Pleas use a different username.')
